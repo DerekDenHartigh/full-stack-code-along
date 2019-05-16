15c986152cb2af2b51e2ef03e7c0aa2bdc2a6bfd
Derek <derekdenhartigh@gmail.com>
2019-05-16 19:09:50 -0400

    running a static public directory - the front-end has met the backend!

--- server.js
+++ server.js
@@ -4,12 +4,15 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use(express.json());
-// app.use("/",students);
+app.use(express.json()); // converts body & params to JSON
 
+app.use(express.static("./public")); // allows access to public directory which has front-end business
+
+// currently the res.send is overwritten by the express.static
 app.get("/", (req, res)=>{
     res.send("Shalome in the home.");
 })
+
 
 // runs the server
 app.listen(port, ()=> console.log(`listening on port: http://localhost:${port}`));
