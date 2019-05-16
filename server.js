bb148dba06273164d4c395179c239a7fdd5fb261
Derek <derekdenhartigh@gmail.com>
2019-05-16 18:53:47 -0400

    set up the server.js, working on setting up app directory

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
 "use strict";
 
 const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+// app.use("/",students);
+
+app.get("/", (req, res)=>{
+    res.send("Shalome in the home.");
+})
+
+// runs the server
+app.listen(port, ()=> console.log(`listening on port: http://localhost:${port}`));
